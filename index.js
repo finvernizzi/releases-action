f602463913c464925531849204e9bb11616c1c37
Fabrizio Invernizzi <fabrizio.invernizzi@quandopasso.com>
2021-04-25 10:14:44 +0200

    Await error

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const github = require('@actions/github');
 const fs = require('fs');
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const path = core.getInput('path');
   console.log(`Reading releases meta info from  ${path}!`);
-  const metaInfo = await fs.readFileSync(path, {encoding:'utf8'});
+  const metaInfo = fs.readFileSync(path, {encoding:'utf8'});
   core.info(`metainfo:`,metaInfo);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
