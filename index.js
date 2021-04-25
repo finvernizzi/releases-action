071c193f258d46c2832b457d04efd009bf94b9b1
Fabrizio Invernizzi <fabrizio.invernizzi@quandopasso.com>
2021-04-25 10:00:23 +0200

    Path

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ try {
   // `who-to-greet` input defined in action metadata file
   const path = core.getInput('path');
   console.log(`Reading releases meta info from  ${path}!`);
-  const metaInfo = fs.readFileSync(`${path}/releases/releases.yml`,{encoding:'utf8'});
+  const metaInfo = fs.readFileSync(`${path}/releases.yml`,{encoding:'utf8'});
   core.info(`metainfo:`,metaInfo);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
