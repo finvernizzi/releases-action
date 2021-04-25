8ecd1c297800a97fa08f6b4bd11cd4a7e764b3a5
Fabrizio Invernizzi <fabrizio.invernizzi@quandopasso.com>
2021-04-25 10:19:25 +0200

    Simplified

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const fs = require('fs');
+'use strict'
 
-try {
-  const path = core.getInput('path');
-  console.log(`Reading releases meta info from  ${path}!`);
-  const metaInfo = fs.readFileSync(path, {encoding:'utf8'});
-  core.info(`metainfo:`,metaInfo);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  core.info(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+const core = require('@actions/core')
+const { promises: fs } = require('fs')
+
+const main = async () => {
+  const path = core.getInput('path')
+  const content = await fs.readFile(path, 'utf8')
+  core.setOutput('content', content)
 }
+
+main().catch(err => core.setFailed(err.message))
